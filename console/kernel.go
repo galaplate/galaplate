77cad2c8e5ccd7aac61b9059e7303b1e27d1e6f2
sheenazien8 <sheenazien08@gmail.com>
2025-09-03 17:15:38 +0700

    feat(console): forgot takeout mising console

--- console/kernel.go
+++ console/kernel.go
@@ -2,11 +2,11 @@ package console
 
 import (
 	"github.com/galaplate/core/console"
-	"github.com/galaplate/galaplate/console/commands"
+	// "github.com/galaplate/galaplate/console/commands"
 )
 
 func RegisterCommands(kernel *console.Kernel) {
     // Register your custom console commands here
     // Example:
-    kernel.Register(&commands.SendwelcomeemailcommandCommand{})
+    // kernel.Register(&commands.SendwelcomeemailcommandCommand{})
 }

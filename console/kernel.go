5236f29cb1ecf101ecaaf92f9352fe328d05a3b8
sheenazien8 <sheenazien08@gmail.com>
2026-08-18 16:13:52 +0700

    add new routelist commands

--- console/kernel.go
+++ console/kernel.go
@@ -2,6 +2,8 @@ package console
 
 import (
 	"github.com/galaplate/core/console"
+	"github.com/galaplate/core/console/commands"
+	"github.com/galaplate/galaplate/router"
 	// "github.com/galaplate/galaplate/console/commands"
 )
 
@@ -9,4 +11,10 @@ func RegisterCommands(kernel *console.Kernel) {
 	// Register your custom console commands here
 	// Example:
 	// kernel.Register(&commands.SendwelcomeemailcommandCommand{})
+	// Inject app-specific router into the built-in route:list command
+	if cmd, ok := kernel.GetCommands()["route:list"]; ok {
+		if routeCmd, ok := cmd.(*commands.RouteListCommand); ok {
+			routeCmd.SetupRoutes = router.SetupRouter
+		}
+	}
 }
